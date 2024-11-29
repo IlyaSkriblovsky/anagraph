c8fed9cbb6a166d2151429727969b45070075c84
Daniil <d.macokin@microline.ru>
2024-11-29 11:02:06 +0300

    feat: add onChangeBoundsEnd

--- src/lib/Manipulator.tsx
+++ src/lib/Manipulator.tsx
@@ -1,21 +1,31 @@
-import { CSSProperties, useState } from "react";
+import { CSSProperties, useState, useCallback } from "react";
 import { useDragAndZoom } from "./useDragAndZoom";
 import { useBoundsContext } from "./BoundsManager";
+import {Bounds} from "./basic-types";
+import { useLatest } from "react-use";
 
 interface ManipulatorProps {
     style: CSSProperties;
     onHover?: (x: number, event: PointerEvent) => void;
     onHoverEnd?: () => void;
     onTouchUp?: (x: number, event: PointerEvent) => void;
+    onChangeBoundsEnd?: (bounds: Bounds) => void;
 }
 
 export function Manipulator(props: ManipulatorProps) {
-    const { onHover, onHoverEnd, onTouchUp } = props;
+    const { onHover, onHoverEnd, onTouchUp, onChangeBoundsEnd } = props;
     const { xBoundsLimit, xBoundsMinVisible, settledXBounds, onManipulation, onManipulationEnd } = useBoundsContext();
 
     const [glass, setGlass] = useState<HTMLDivElement | null>(null);
 
-    useDragAndZoom(glass, settledXBounds, onManipulation, onManipulationEnd, onHover, onHoverEnd, onTouchUp, {
+    const latestChangeBoundsEnd = useLatest(onChangeBoundsEnd);
+
+    const onEnd = useCallback((bounds: Bounds) => {
+        onManipulationEnd(bounds);
+        latestChangeBoundsEnd.current?.(bounds);
+    }, [onManipulationEnd])
+
+    useDragAndZoom(glass, settledXBounds, onManipulation, onEnd, onHover, onHoverEnd, onTouchUp, {
         boundsLimit: xBoundsLimit,
         xMinVisible: xBoundsMinVisible,
     });
