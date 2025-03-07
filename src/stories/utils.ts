87e23849d74e4d1f2a43fb18fd9098ef8cae117c
Daniil <d.macokin@microline.ru>
2025-03-07 13:42:19 +0300

    hotfix: add worker fallback

--- src/stories/utils.ts
+++ src/stories/utils.ts
@@ -1,3 +1,16 @@
+import AnagraphChartWorker from "./storybook-worker";
+import { createFallback } from "../lib/index";
+
 export function ts(year: number, month: number, day: number, hour: number = 0, minute: number = 0, second: number = 0) {
     return new Date(year, month, day, hour, minute, second).getTime();
+}
+
+export function getCreateWorkerOrFallback(): () => Worker {
+    const isSupport = HTMLCanvasElement.prototype.transferControlToOffscreen !== undefined;
+
+    if (!isSupport) {
+        return () => createFallback();
+    }
+
+    return () => new AnagraphChartWorker();
 }
