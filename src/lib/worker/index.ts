61e3b019e01187d1571288031e4bcf45b4ab3061
Daniil <d.macokin@microline.ru>
2025-03-11 10:21:34 +0300

    ref: refactor startWorker & createFallback

--- src/lib/worker/index.ts
+++ src/lib/worker/index.ts
@@ -33,7 +33,7 @@ function handleObjectMessages<K extends string, O extends object>(
     return false;
 }
 
-export function startWorker() {
+function startWorkerOrCreateFallback(isWorker: boolean) {
     let drawContext: DrawContext | null = null;
 
     const chartInfo: ChartInfo = {
@@ -60,15 +60,7 @@ export function startWorker() {
         });
     }
 
-    setInterval(() => {
-        const now = new Date().getTime();
-        const fps = (framesDrawn / (now - lastDrawTime)) * 1e3;
-        lastDrawTime = now;
-        framesDrawn = 0;
-        postMessage(statsReportMessage(fps));
-    }, 1e3);
-
-    addEventListener("message", (msg: MessageEvent<MainToWorkerMessage>) => {
+    function resolveMessage(msg: MessageEvent<MainToWorkerMessage>) {
         if (chartInfo.settings._verbose) {
             console.log(
                 "WORKER MSG",
@@ -84,7 +76,10 @@ export function startWorker() {
             case "setCanvas": {
                 const { canvas, devicePixelRatio } = msg.data;
                 if (canvas) {
-                    const ctx = canvas.getContext("2d", { desynchronized: true });
+                    const ctx = canvas.getContext("2d", { desynchronized: true }) as
+                        | CanvasRenderingContext2D
+                        | OffscreenCanvasRenderingContext2D
+                        | null;
                     if (ctx) {
                         drawContext = {
                             canvas,
@@ -143,146 +138,47 @@ export function startWorker() {
         }
 
         assertNever(msg.data);
-    });
+    }
+
+    if (!isWorker) {
+        return {
+            postMessage(data) {
+                setTimeout(() => {
+                    resolveMessage({ data: data, type: data.type } as MessageEvent);
+                }, 0);
+            },
+            onmessage: null,
+            terminate() {},
+            addEventListener<K extends keyof WorkerEventMap>(
+                type: K,
+                listener: (this: Worker, ev: WorkerEventMap[K]) => any,
+                options?: boolean | AddEventListenerOptions,
+            ) {},
+            removeEventListener<K extends keyof WorkerEventMap>(
+                type: K,
+                listener: (this: Worker, ev: WorkerEventMap[K]) => any,
+                options?: boolean | EventListenerOptions,
+            ) {},
+        } as Worker;
+    }
+
+    setInterval(() => {
+        const now = new Date().getTime();
+        const fps = (framesDrawn / (now - lastDrawTime)) * 1e3;
+        lastDrawTime = now;
+        framesDrawn = 0;
+        postMessage(statsReportMessage(fps));
+    }, 1e3);
+
+    addEventListener("message", resolveMessage);
+
+    return;
+}
+
+export function startWorker(): undefined {
+    return startWorkerOrCreateFallback(true) as undefined;
 }
 
 export function createFallback(): Worker {
-    let drawContext: DrawContext | null = null;
-
-    const chartInfo: ChartInfo = {
-        settings: defaultChartSettings,
-        xBounds: [0, 1],
-        lines: new Map<Id, LineInfo>(),
-        verticalFillings: new Map<Id, VerticalFilling>(),
-        bottomStatuses: new Map<Id, BottomStatus>(),
-    };
-
-    let framesDrawn = 0;
-    let lastDrawTime = 0;
-    let drawPlanned = false;
-    function planRedraw() {
-        if (drawPlanned) return;
-
-        drawPlanned = true;
-        requestAnimationFrame(() => {
-            if (drawContext) {
-                drawChart(drawContext, chartInfo);
-                framesDrawn++;
-            }
-            drawPlanned = false;
-        });
-    }
-
-    setInterval(() => {
-        const now = new Date().getTime();
-        const fps = (framesDrawn / (now - lastDrawTime)) * 1e3;
-        lastDrawTime = now;
-        framesDrawn = 0;
-        postMessage(statsReportMessage(fps));
-    }, 1e3);
-
-    const fu = (msg: MessageEvent<MainToWorkerMessage>) => {
-        if (chartInfo.settings._verbose) {
-            console.log(
-                "WORKER MSG",
-                msg.data.type,
-                msg.data.type.startsWith("change")
-                    ? Object.keys((msg.data as unknown as any).attrs ?? {})
-                    : msg.data.type === "setXBoundsAndRedraw"
-                    ? msg.data
-                    : undefined,
-            );
-        }
-
-        if (!msg?.data?.type) {
-            return;
-        }
-
-        switch (msg.data.type) {
-            case "setCanvas": {
-                const { canvas, devicePixelRatio } = msg.data;
-                if (canvas) {
-                    const ctx = canvas.getContext("2d", { desynchronized: true });
-                    if (ctx) {
-                        drawContext = {
-                            canvas,
-                            ctx,
-                            devicePixelRatio,
-                        };
-                    } else {
-                        drawContext = null;
-                    }
-                }
-                if (drawContext) {
-                    drawContext.devicePixelRatio = msg.data.devicePixelRatio;
-                }
-                planRedraw();
-                return;
-            }
-
-            case "setCanvasSize": {
-                if (drawContext) {
-                    drawContext.canvas.width = msg.data.width;
-                    drawContext.canvas.height = msg.data.height;
-                    planRedraw();
-                }
-                return;
-            }
-
-            case "setXBoundsAndRedraw": {
-                chartInfo.xBounds = msg.data.xBounds;
-                planRedraw();
-                return;
-            }
-
-            case "setChartSettings": {
-                chartInfo.settings = msg.data.chartSettings;
-                return;
-            }
-        }
-
-        if (isEditObjectMessage("Line", msg.data)) {
-            if (handleObjectMessages("Line", msg.data, chartInfo.lines)) {
-                planRedraw();
-            }
-            return;
-        }
-        if (isEditObjectMessage("VerticalFilling", msg.data)) {
-            if (handleObjectMessages("VerticalFilling", msg.data, chartInfo.verticalFillings)) {
-                planRedraw();
-            }
-            return;
-        }
-        if (isEditObjectMessage("BottomStatus", msg.data)) {
-            if (handleObjectMessages("BottomStatus", msg.data, chartInfo.bottomStatuses)) {
-                planRedraw();
-            }
-            return;
-        }
-
-        assertNever(msg.data);
-    };
-
-    return {
-        postMessage(data) {
-            setTimeout(() => {
-                // @ts-ignore
-                fu({ data: data, type: data.type });
-            }, 0);
-        },
-        onmessage: null,
-        terminate() {
-            console.debug("terminate");
-        },
-        addEventListener<K extends keyof WorkerEventMap>(
-            type: K,
-            listener: (this: Worker, ev: WorkerEventMap[K]) => any,
-            options?: boolean | AddEventListenerOptions,
-        ) {},
-        removeEventListener<K extends keyof WorkerEventMap>(
-            type: K,
-            listener: (this: Worker, ev: WorkerEventMap[K]) => any,
-            options?: boolean | EventListenerOptions,
-        ) {},
-    } as Worker;
+    return startWorkerOrCreateFallback(false) as Worker;
 }
